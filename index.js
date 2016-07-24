06ba124f5f0d962e286f0e499b78c3bed578fbe1
Frederic Gingras <frederic@gingras.cc>
2016-07-24 16:24:01 -0400

    v4

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var eth = require('eth');
 module.exports = function(source) {
   this.cacheable && this.cacheable();
 
-  return eth.write(eth.read(this.resourcePath, source));
+  return eth.indent(eth.compile(source));
 };
